2935c763debdfdb4aeb1c2715a23e488fa765489
olyemicheal75-rgb <olyemicheal75@gmail.com>
2026-07-04 15:14:35 +0100

    Add bcrypt password manager utility

---
+++ src/utils/passwordManager.js
@@ -0,0 +1,17 @@
+const bcrypt = require('bcryptjs');
+
+const SALT_ROUNDS = 10;
+
+const hashPassword = async (password) => {
+  const salt = await bcrypt.genSalt(SALT_ROUNDS);
+  return bcrypt.hash(password, salt);
+};
+
+const comparePassword = async (password, hash) => {
+  return bcrypt.compare(password, hash);
+};
+
+module.exports = {
+  hashPassword,
+  comparePassword,
+};
